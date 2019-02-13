b5efed8c24cdf43079bd5e9ed268598a90f40f66
Vasyl Khmil <vasylkhmil1995@gmail.com>
2019-02-13 17:23:01 +0200

    - fix name bugs

--- Brew/Presentation Logic/General/Views/UITableViewCell/PodcastSearchTableViewCell/PodcastSearchTableViewCell.swift
+++ Brew/Presentation Logic/General/Views/UITableViewCell/PodcastSearchTableViewCell/PodcastSearchTableViewCell.swift
@@ -2,7 +2,7 @@
 //  PodcastSearchTableViewCell.swift
 //  Brew
 //
-//  Created by Andriy Vahniy on 2/13/19.
+//  Created by Vasyl Khmil on 2/13/19.
 //  Copyright © 2019 NerdzLab. All rights reserved.
 //
 
